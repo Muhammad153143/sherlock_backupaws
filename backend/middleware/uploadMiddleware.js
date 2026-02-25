d6ce6ea5066c2014645eea6e83e522cc1ff94ee1
Muhammad153143 <skjuber0004@gmail.com>
2026-02-25 15:06:31 +0530

    Update Cloudinary configuration and folder name

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,13 +1,19 @@
-const multer = require("multer");
-const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../utils/cloudinary");
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "sherlock",
-    allowed_formats: ["jpg", "jpeg", "png"],
-  },
+    cloudinary: cloudinary,
+    params: {
+        folder: 'sherlock-items',
+        allowed_formats: ['jpg', 'jpeg', 'png'],
+    },
 });
 
 const upload = multer({ storage });
