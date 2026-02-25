ef73147cf543ef0997292ba439d4976ea7b14dc4
Muhammad153143 <skjuber0004@gmail.com>
2026-02-25 12:48:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const { initAIService } = require('./utils/aiService');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.use(helmet({
     contentSecurityPolicy: false,
