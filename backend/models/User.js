3492a3aa3a39f58f7532457eee7e18128f0698a9
Muhammad153143 <skjuber0004@gmail.com>
2026-03-07 16:23:18 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -44,10 +44,15 @@ const userSchema = new mongoose.Schema({
 });
 
 // Encrypt password using bcrypt
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+userSchema.pre("save", async function (next) {
 
-    this.password = await bcrypt.hash(this.password, 12);
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
     next();
 });
 // Match user entered password to hashed password in database
