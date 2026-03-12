9e572fb694a2071ff172c433d65269626b844775
Muhammad153143 <skjuber0004@gmail.com>
2026-03-12 08:57:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -89,7 +89,7 @@ mongoose.connect(process.env.MONGO_URI)
                     'Environment': 'Production'
                 },
                 actionText: 'Go to Dashboard',
-                actionUrl: 'https://sherlock-lost-and-found.onrender.com'
+                actionUrl: 'https://sherlock-lost-and-found3-ins8anbb0-muhammad153143s-projects.vercel.app/admin-dashboard.html'
             },
             type: 'system_notification',
             triggeredBy: null
