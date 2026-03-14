a95c2bf434e19b6d17bba5f245e7f16763ee1d33
Muhammad153143 <skjuber0004@gmail.com>
2026-03-14 06:32:48 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.post('/v1/register', register);
-router.post('/v1/login', login);
+router.post('/register', register);
+router.post('/login', login);
 router.get('/me', protect, getMe);
-router.post('/v1/forgot-password', forgotPassword);
-router.post('/v1/reset-password/:token', resetPassword);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
 
 module.exports = router;
