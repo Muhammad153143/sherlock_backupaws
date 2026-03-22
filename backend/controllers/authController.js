e1b60e89045bb662ac30d4ddfa7395de62dd4b31
Muhammad153143 <skjuber0004@gmail.com>
2026-03-22 22:09:56 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,10 +14,12 @@ const generateToken = (id) => {
 // @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = async (req, res) => {
-    const { name, email, password } = req.body;
+    const { name, email, username, password } = req.body;
 
     try {
-        const userExists = await User.findOne({ email });
+        const userExists = await User.findOne({
+            $or: [{ email }, { username }]
+        });
 
         if (userExists) {
             return res.status(400).json({ message: 'User already exists' });
@@ -26,16 +28,19 @@ exports.register = async (req, res) => {
         const user = await User.create({
             name,
             email,
+            username,   // ✅ ADD THIS
             password
-        })
+        });
 
         res.status(201).json({
             _id: user._id,
             name: user.name,
+            username: user.username,
             email: user.email,
             role: user.role,
             token: generateToken(user._id)
         });
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
